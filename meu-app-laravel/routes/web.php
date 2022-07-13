c96c14878aabcee49740178fa52f930161e87357
vinic-alves <vinicius.alves95@outlook.com>
2022-07-13 20:33:40 -0300

    Migrations

--- meu-app-laravel/routes/web.php
+++ meu-app-laravel/routes/web.php
@@ -1,11 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\{
+    UserController,
+    ViaCepController
+};
+
 
 
 #abaixo uma rota com parametro
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
 Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
 //metodo     //rota     //controller               //metodo     'nome-da-rota'
+
+
+// Rotas do Via Cep
 Route::get('/viacep', [ViaCepController::class, 'index'])->name('viacep.index');
+Route::post('/viacep', [ViaCepController::class, 'index'])->name('viacep.index.post');
+Route::get('/viacep/{cep}', [ViaCepController::class, 'show'])->name('viacep.show');
