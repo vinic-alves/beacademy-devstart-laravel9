4d40ab7d7433f5fd27f5c66a22ee8e1a85ee5600
vinic-alves <vinicius.alves95@outlook.com>
2022-07-12 22:13:21 -0300

    Utilizado API

--- meu-app-laravel/routes/web.php
+++ meu-app-laravel/routes/web.php
@@ -7,3 +7,5 @@ use App\Http\Controllers\UserController;
 #abaixo uma rota com parametro
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
 Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
+//metodo     //rota     //controller               //metodo     'nome-da-rota'
+Route::get('/viacep', [ViaCepController::class, 'index'])->name('viacep.index');
