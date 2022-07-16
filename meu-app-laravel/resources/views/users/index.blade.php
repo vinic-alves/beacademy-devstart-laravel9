1e0b2a5c5455eddb1e27d87e7add34794d7568b6
vinic-alves <vinicius.alves95@outlook.com>
2022-07-16 11:46:08 -0300

    Aula de blade

--- meu-app-laravel/resources/views/users/index.blade.php
+++ meu-app-laravel/resources/views/users/index.blade.php
@@ -1,14 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Listagem de usuários</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
-</head>
-<body>
-    <div class="container">
+@extends('template.users')
+@section('title', 'Listagem de Usuários')
+@section('body')
+
         <h1>Listagem de Usuários</h1>
             <table class="table">
             <thead>
@@ -33,6 +26,5 @@
                 @endforeach
             </tbody>
             </table>
-    </div>
-</body>
-</html>
+
+@endsection
--- meu-app-laravel/resources/views/users/show.blade.php
+++ meu-app-laravel/resources/views/users/show.blade.php
@@ -1,16 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Usuário {{ $user->name }}</title>
-</head>
-<body>
-<div class="container">
-        <h1>Usuário {{ $user->name }}</h1>
+@extends('template.users')
+@section('title', 'Visualizar Usuário')
+@section('body')
+
+        <h1>Listagem de Usuários</h1>
             <table class="table">
-            <thead>
+            <thead class="text-center">
                 <tr>
                 <th scope="col">Id</th>
                 <th scope="col">Nome</th>
@@ -18,11 +12,10 @@
                 <th scope="col">Data Cadastro</th>
                 <th scope="col">Ações</th>
                 
-                
                 </tr>
             </thead>
-            <tbody>
-                
+            <tbody class="text-center">
+                @foreach($users as $user)
                     <tr>
                     <th scope="row">{{ $user->id }}</th>
                     <td>{{ $user->name}}</td>
@@ -30,12 +23,11 @@
                     <td>{{ date('d/m/Y - H:i', strtotime($user->created_at))}}</td>
                     <td> 
                         <a href="" class="btn btn-warning text-white">Editar</a>
-                        <a href="" class="btn btn-danger text-white">Deletar</a> 
+                        <a href="" class="btn btn-danger text-white">Deletar</a>
                     </td>
                     </tr>   
-                
+                @endforeach
             </tbody>
             </table>
-    </div>
-</body>
-</html>
+
+@endsection
