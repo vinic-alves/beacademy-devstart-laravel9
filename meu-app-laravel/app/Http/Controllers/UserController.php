8a7a2dd546cd6507bc50991404edec1f8ae9c2a3
vinic-alves <vinicius.alves95@outlook.com>
2022-07-17 17:09:22 -0300

    Metodo de deletar

--- meu-app-laravel/app/Http/Controllers/UserController.php
+++ meu-app-laravel/app/Http/Controllers/UserController.php
@@ -78,6 +78,16 @@ class UserController extends Controller
 
         return redirect()->route('users.index');
     }
+    public function destroy()
+    {
+        if(!$user = $this->model->find($id))
+            return redirect()->route('users.index');
+        
+        $user->delete();
+
+        return redirect()->route('users.index');
+
+    }
 }
 
 
--- meu-app-laravel/routes/web.php
+++ meu-app-laravel/routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\{
 
 
 #abaixo uma rota com parametro
+Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
 Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
 Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
 Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
